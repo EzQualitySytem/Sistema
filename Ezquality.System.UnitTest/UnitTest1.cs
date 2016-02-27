7b09045adbd80e889954ee446c92264f4668b053
Ricardo da Silva <ricardocaosilva@hotmail.com>
2016-02-27 18:26:28 -0300

    Versão 0.0

---
+++ Ezquality.System.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ezquality.System.UnitTest
+{
+    [TestClass]
+    public class UnitTestEZQuality
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Ezquality.System.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ezquality.System.Web.MvcApplication" Language="C#" %>
